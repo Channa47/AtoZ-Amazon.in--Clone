b6408b2c069a2b94f47da03832dba8b8bdd22342
Channa47 <103635080+Channa47@users.noreply.github.com>
2023-01-17 17:04:35 +0530

    fakeDataupdated

---
+++ FrontEnd/atoz/src/Admin/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class AdminPage extends Component {
+  render() {
+    return (
+      <div>AdminPage</div>
+    )
+  }
+}
+
+export default AdminPage
