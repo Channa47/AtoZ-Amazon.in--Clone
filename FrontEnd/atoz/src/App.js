421262b4333c1edb96d55b0d678efa4f00e1461e
Channa47 <103635080+Channa47@users.noreply.github.com>
2023-01-20 13:31:00 +0530

    AdminRoute

--- FrontEnd/atoz/src/App.js
+++ FrontEnd/atoz/src/App.js
@@ -2,12 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import AdminPage from "./Admin/AdminPage";
 import AllRoutes from "./Routes/AllRoutes";
-import Simple from "./Admin/Sample";
+import Sample from "./Admin/Sample"
 function App() {
   return (
     <div className="App">
       {/* <AdminPage/> */}
-      {/* <Simple /> */}
+      {/* <Sample /> */}
       <AllRoutes />
     </div>
   );
--- FrontEnd/atoz/src/Routes/AllRoutes.jsx
+++ FrontEnd/atoz/src/Routes/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import { Routes, Route } from "react-router-dom";
+import Sample from "../Admin/Sample";
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/register" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Sample/>}/>
       </Routes>
     </div>
   );
